bbc82e13f397f93244762c47aed03cb9503ab8f2
nistha-singh <nistha20003@gmail.com>
2024-03-08 20:57:08 +0530

    navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import "../public/logo.png"
 import Image from "next/image"
+import Link from "next/link"
+import { Russo_One } from 'next/font/google'
+const russo = Russo_One({ subsets: ["latin"],weight: '400',});
+
 const Navbar = () => {
   return (
     <>
-    <div className="flex justify-between w-full h-16 bg-green-100">
-        <Image src="/logo.png" width="200" height="50" className="p-4"/>
-        <div className="flex p-4 pr-8 gap-4">
-            <span>Home</span>
-            <span>About Us</span>
-            <span>Contact Us</span>
-            <span>Discover</span>
-            <span>Login</span>
+    <div className="flex justify-between w-full h-16 bg-gradient-to-r from-light-green to-greenish">
+        <Image src="/logo.png" width="250" height="50" className="p-2 pl-4"/>
+        <div className="flex items-center p-4 pr-8 gap-4">
+            <Link href="/" className={russo.className}><p className="text-darkish-green no-underline hover:underline">Home</p></Link>
+            <Link href="/aboutus" className={russo.className}><p className="text-darkish-green no-underline hover:underline">About Us</p></Link>
+            <Link href="/contactus" className={russo.className}><p className="text-darkish-green no-underline hover:underline">Contact Us</p></Link>
+            <Link href="/discover" className={russo.className}><p className="text-darkish-green no-underline hover:underline">Discover</p></Link>
+            <Link href="/login" className={russo.className}><p className="p-1 rounded-md text-greenish bg-navy-blue no-underline hover:underline">Login</p></Link>
         </div>
     </div>
     </>
